364af39430ea34ee07b0399a3c77c5305863e140
Advait <advait062007@gmail.com>
2026-07-14 22:27:51 +0530

    ex1

---
+++ Assignment2/exercise1.java
@@ -0,0 +1,31 @@
+package Assignment2;
+
+class Student{
+    String name;
+    int rollNo;
+
+    Student() {                             //Default Constructor
+        name = "Default";
+        rollNo = 0;
+        System.out.println("Default COnstructor called.");
+    }
+    
+    Student(String n, int r) {              //Parameterized Constructor
+         name = n;
+         rollNo = r;
+         System.out.println("Parameterized COnstructor called.");
+    } 
+
+    void display(){
+        System.out.println("Name: "+ name + ", Roll No.: " + rollNo);
+    }
+}
+
+public class exercise1{
+    public static void main(String[] args){
+        Student student1 = new Student();
+        student1.display();
+        Student student2 = new Student("Advait", 23);
+        student2.display();
+    }
+}
