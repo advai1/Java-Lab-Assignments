6d03791dff381565d0286dab4ed9541bc4e72bac
Advait <advait062007@gmail.com>
2026-07-15 11:11:56 +0530

    hierarchical

---
+++ Assignment3/HierarchicalInheritance.java
@@ -0,0 +1,31 @@
+package Assignment3;
+
+class Animal {
+    void eat() {
+        System.out.println("This animal eats food.");
+    }
+}
+
+class Dog extends Animal {
+    void bark(){
+        System.out.println("Dog Barks!");
+    }
+}
+
+class Cat extends Animal {
+    void meow(){
+        System.out.println("Cat Meows!");
+    }
+}
+
+public class HierarchicalInheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+
+        Cat c = new Cat();
+        c.eat();
+        c.meow();
+    }
+}
