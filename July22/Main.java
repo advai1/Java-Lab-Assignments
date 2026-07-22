5a8bf089a4b41608fe9e08f4d92407c00fb7e5f8
Advait <advait062007@gmail.com>
2026-07-22 11:17:42 +0530

    package july 22

---
+++ July22/Main.java
@@ -0,0 +1,10 @@
+package July22;
+
+import July22.mypack.MyClass;
+
+public class Main {
+    public static void main(String[] args){
+        MyClass obj = new MyClass();
+        obj.display();
+    }
+}
---
+++ July22/mypack/MyClass.java
@@ -0,0 +1,7 @@
+package July22.mypack;
+
+public class MyClass {
+    public void display(){
+        System.out.println("This is a package example.");
+    }
+}
