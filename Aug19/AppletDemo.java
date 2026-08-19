5d6f9107b83214cb0ea2272de51e89fa59f92980
Advait <advait062007@gmail.com>
2026-08-19 11:32:00 +0530

    swing and applet added

---
+++ Aug19/AppletDemo.java
@@ -0,0 +1,13 @@
+package Aug19;
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class AppletDemo {
+    public static void main(String[] args) {
+        g.drawString("Welcome to Java Applet!", 50, 50);
+    }
+}
+
+/*
+
+*/
